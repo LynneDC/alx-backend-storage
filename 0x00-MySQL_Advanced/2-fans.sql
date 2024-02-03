bd53045477578c823a0334ddc5ad0c4324638ddf
LynneDC <roselinedc1308@gmail.com>
2024-02-03 13:07:11 +0000

    change count and replace with sum to get total sum

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- importing table and count the number of fans per origin
 
-SELECT origin, COUNT(fans) as nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
