f01e50843f005e20d0b75263aa47390def3338f5
LynneDC <roselinedc1308@gmail.com>
2024-02-03 13:25:06 +0000

    fix syntax error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,5 @@
---ists all bands with Glam rock ranked by their longevity
-
-SELECT band_name 
-       (IFNULL(split, 2022) - formed) AS lifespan
+-- ists all bands with Glam rock ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style = %Glam Rock%
-ORDER BY lifespan DESC;
+WHERE style LIKE "%Glam Rock%"
+ORDER BY lifespan DESC
