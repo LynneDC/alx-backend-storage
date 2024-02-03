b4306b6f152c74cd585c2b7e23423268da42ade1
LynneDC <roselinedc1308@gmail.com>
2024-02-03 12:44:20 +0000

    fix synrax error at 6

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 -- creates table and uses enum to restrict country to US, CO, or TN.
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
+
