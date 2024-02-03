92cb67680c06b59b9e3efe3e298282bcafdbdcef
LynneDC <roselinedc1308@gmail.com>
2024-02-03 12:08:48 +0000

    script that create database if it doesnt exist with speified requirements

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -9,8 +9,8 @@ script should execute on any database
 make attribute unique directly in the table
 */
 
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
     );
