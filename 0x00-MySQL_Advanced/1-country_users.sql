f1cf95462d4644cc4718369f586f379db71055e2
LynneDC <roselinedc1308@gmail.com>
2024-02-03 14:23:27 +0200

    enumartion of countries and retsriction

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,8 @@
--- create a script with default as first element of enumaration.
+-- creates table and uses enum to restrict country to US, CO, or TN.
 
 CREATE TABLE IF NOT EXISTS users (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 );
